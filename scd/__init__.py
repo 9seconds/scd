c7a2b035c56da1f2c5e0f9bd8b8a1ca9249670e8
9seconds <nineseconds@yandex.ru>
2016-12-26 23:02:09 +0300

    Eat own dog food

--- scd/__init__.py
+++ scd/__init__.py
@@ -12,3 +12,8 @@ except ImportError:
     pass
 else:
     colorama.init(autoreset=True)
+
+
+# TODO(9seconds): Make paths in config files idempotent to OS
+__version__ = "0.1.0"
+__version__ = __version__.split(".")
